010464ecdde57cce1c6b33b46c309c47cee09dc2
TSI_develop imac <smaregi_medical@icloud.com>
2022-08-04 11:24:59 +0900

    Initial Commit

---
+++ OuterKeybordWatch/OuterKeybordWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OuterKeybordWatchApp.swift
+//  OuterKeybordWatch
+//
+//  Created by 株式会社ティーエスアイ on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct OuterKeybordWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
